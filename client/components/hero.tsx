86160eb4347881f4e8140e836963b8ba4f8a9012
bread133 <bread133@yandex.ru>
2024-06-16 14:24:22 +0300

    feat: rename title in client

--- client/components/hero.tsx
+++ client/components/hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
       <div className='container flex flex-col items-center justify-between h-full max-w-6xl px-8 mx-auto -mt-32 lg:flex-row xl:px-0'>
         <div className='z-30 flex flex-col items-center w-full max-w-xl pt-48 text-center lg:items-start lg:w-1/2 lg:pt-20 xl:pt-40 lg:text-left'>
           <h1 className='relative mb-4 text-3xl font-black leading-tight text-gray-900 sm:text-6xl xl:mb-8'>
-            Xk7-retail-group без жены Родиона и Катьки
+            Xk7-retail-group
           </h1>
           <p className='pr-0 mb-8 text-base text-gray-600 sm:text-lg xl:text-xl lg:pr-20'>
             Сервис проверки видеофайлов на нарушение авторских прав
