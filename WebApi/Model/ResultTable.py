014132e0b6e5cac2aeb5d9d119f2cce185b155f0
bread133 <bread133@yandex.ru>
2024-06-23 11:59:06 +0300

    feat: add str in class for json form (in process)

--- WebApi/Model/ResultTable.py
+++ WebApi/Model/ResultTable.py
@@ -1,6 +1,6 @@
 from typing import List
 from pydantic import BaseModel
-
+from varname import nameof
 
 class IVideoBorrowing(BaseModel):
     title_license: str
@@ -10,11 +10,21 @@ class IVideoBorrowing(BaseModel):
     time_piracy_start: int
     time_piracy_finish: int
 
+    def __str__(self):
+        return (f"{nameof(self.title_license)}: {self.title_license},\n"
+                f"")
+
 
 class IResponseServerUploadFiles(BaseModel):
     message: str
     status: int
     borrowing: List[IVideoBorrowing]
+
+    def __str__(self):
+        return (f"{nameof(self.message)}: {self.message},\n"
+                f"{nameof(self.status)}: {self.status},\n"
+                f"{nameof(self.borrowing)}: {self.borrowing}")
+
 
 
 def to_json(upload_files_response: IResponseServerUploadFiles):
