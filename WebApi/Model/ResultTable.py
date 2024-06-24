be296d2ad7d4e221bc83a3e834b539b3c9e9c542
bread133 <bread133@yandex.ru>
2024-06-24 07:24:52 +0300

    feat: add json-output

--- WebApi/Model/ResultTable.py
+++ WebApi/Model/ResultTable.py
@@ -12,7 +12,14 @@ class IVideoBorrowing(BaseModel):
 
     def __str__(self):
         return (f"{nameof(self.title_license)}: {self.title_license},\n"
-                f"")
+                f"{nameof(self.title_piracy)}: {self.title_piracy},\n"
+                f"{nameof(self.title_license_start)}: {self.title_license_start},\n"
+                f"{nameof(self.title_license_finish)}: {self.title_license_finish},\n"
+                f"{nameof(self.title_piracy_start)}: {self.title_piracy_start},\n"
+                f"{nameof(self.title_piracy_finish)}: {self.title_piracy_finish},\n")
+
+    def to_json(self):
+        self.to_json()
 
 
 class IResponseServerUploadFiles(BaseModel):
@@ -24,6 +31,9 @@ class IResponseServerUploadFiles(BaseModel):
         return (f"{nameof(self.message)}: {self.message},\n"
                 f"{nameof(self.status)}: {self.status},\n"
                 f"{nameof(self.borrowing)}: {self.borrowing}")
+
+    def to_json(self):
+        self.to_json()
 
 
 
