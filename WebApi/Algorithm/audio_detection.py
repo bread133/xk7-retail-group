709875055485860cc9abf129b7801cd540b85a68
Illarion <pro100illarion@gmail.com>
2024-06-24 08:34:02 +0000

    added coments

--- WebApi/Algorithm/audio_detection.py
+++ WebApi/Algorithm/audio_detection.py
@@ -5,7 +5,15 @@ from default_logger import logger
 
 
 def merge_sequence(duration_list: list[tuple[int, int, int]], value: tuple[int, int, int], idx_start: int, idx_end: int):
-    
+    """
+    Выполняет соединение последовательности с новым элементом (если есть пересечение). 
+
+    :param duration_list: список-кортежей (начало совпадения из БД, конец совпадения из БД, смещение для определения локального времени).
+    :param value: кортеж, который необходимо соединить с некоторой последовательностью.
+    :param idx_start: индекс начала последовательности.
+    :param idx_end: индекс конца последовательности.
+    """
+     
     left_value = min(duration_list[idx_start][0], value[0])
     right_value = max(duration_list[idx_end - 1][1], value[1])
 
@@ -20,6 +28,15 @@ def merge_sequence(duration_list: list[tuple[int, int, int]], value: tuple[int, 
 
 
 def add_duration(duration_list: list[tuple[int, int, int]], value: tuple[int, int, int], max_distance=1):
+    """
+    Выполняет добавление нового промежутка (value) в список промежутков, которые не пересекаются, а также,
+    расстояние между границами, больше чем max_distance. После добавления нового промежутка свойства списка сохрананяются.
+    
+    :param duration_list: список промежутков, представлен в виде списка-кортежей, где элементы:
+        начало совпадения из БД, конец совпадения из БД, смещение для определения локального времени.
+    :param value: промежуток в виде кортежа для добавления в список.
+    :param max_distance: максимальное расстояние между границами для выполнения их объединения (в секундах).
+    """
 
     if not duration_list:
         duration_list.append(value)
@@ -47,6 +64,23 @@ def add_duration(duration_list: list[tuple[int, int, int]], value: tuple[int, in
 
 def find_durations(dict_audio: dict[list[tuple[int, int]]], max_diff_s=1, min_duration_s=10)\
         -> dict[int, list[tuple[int, int, int]]]:
+    """
+    Выполняет поиск промежутков совпадения.
+    Идея: ранжировать время по разности локального и удаленного времени.
+    Ранжирование происходит в словаре data_duration.
+    Ключ это разность локального и удаленного времени, а значение это список удаленных времен.
+    Тогда для каждого ключа есть список, где времена совпадения в порядке возрастения в МС.
+    В таком случае необходимо найти все подпоследовательности, где разность не больше max_diff_s
+    и выполнить их объединения с другими промежутками, если их разность соседних границ не больше max_diff_s.
+    Итоговые последовательности должны быть не меньше min_duration_s.
+    
+    :param dict_audio: словарь аудио совпадений из БД, где ключ - id контента, а значение - список в виде кортежа времен, у которых совпали хеши,
+        первый элемент кортежа - время из БД, второй - время локального аудиофрагмента.
+    :param max_diff_s: максимальное расстояние между границами для выполнения их объединения (в секундах).
+    :param min_duration_s: длительность минимального промежука совпадения (в секундах).
+    :result: промежутки совпадения в виде словаря, где ключ - id контента, а значение - список совпадений в виде
+        кортежа: начало совпадения из БД, конец совпадения из БД, смещение для определения локального времени.
+    """
 
     result_dict = defaultdict(list[tuple[int, int, int]])
 
@@ -55,6 +89,7 @@ def find_durations(dict_audio: dict[list[tuple[int, int]]], max_diff_s=1, min_du
         duration_list = []
         data_duration = defaultdict(list)
 
+        # Ранжируем по разности time2 и time1
         for time1, time2 in values:
             diff = int((time2 - time1) // 1000)
             data_duration[diff].append(time1)
@@ -76,7 +111,9 @@ def find_durations(dict_audio: dict[list[tuple[int, int]]], max_diff_s=1, min_du
                     if (times[j + 1] - times[j]) // 1000 > max_diff_s:
                         break
                     j += 1
+                    
                 if j - i > 2:
+                    
                     new_diff_local = int(diff_local)
                     if times[i] // 1000 + int(diff_local) < 0:
                         new_diff_local += 1
@@ -98,19 +135,33 @@ def find_durations(dict_audio: dict[list[tuple[int, int]]], max_diff_s=1, min_du
 
 
 def detect_audio(hashes_: list[tuple[str, int]], duration_audio, db_service_: DBService, max_diff_s=3, min_duration_s=10) -> dict[int, list[tuple[int, int, int]]]:
+    """
+    Определение совпадений аудио фрагмента из БД
+    
+    :param hashes_: хеши аудио фрагмента для поиска совпадений из БД.
+    :param duration_audio: длительность аудио фрагмента (в секундах).
+    :param db_service_: объект сервиса БД
+    :param max_diff_s: максимальное расстояние между границами для выполнения их объединения (в секундах).
+    :param min_duration_s: длительность минимального промежука совпадения (в секундах).
+    
+    :result: промежутки совпадения в виде словаря, где ключ - id контента, а значение - список совпадений в виде
+        кортежа: начало совпадения из БД, конец совпадения из БД, смещение для определения локального времени.
+    """
 
     result_hashes = db_service_.get_audio_snapshots_by_hashes([values[0] for values in hashes_])
 
+    # Для быстрого поиска по хешу
     hash_table = defaultdict(int)
     for hash_value, timestamp in hashes_:
         hash_table[hash_value] = timestamp
 
+    # Ранжируем по id_content
     dict_audio = defaultdict(list)
     for id_content, timestamp, hash_value in result_hashes:
         dict_audio[id_content].append((timestamp, hash_table[hash_value]))
 
     result_dict = find_durations(dict_audio, max_diff_s=max_diff_s, min_duration_s=min_duration_s)
-    
+     
     for id_content, values in result_dict.items():
         if values[0][0] <= 5:
             values[0] = (0, values[0][1], values[0][2])
